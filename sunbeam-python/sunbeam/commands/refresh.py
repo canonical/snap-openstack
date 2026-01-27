9360c831ad83a4d612acdad5de3766699e7eadc2
Raven Kaur <raven.kaur@canonical.com>
2026-01-27 01:32:41 -0500

    feat: adds mysql-k8s charm upgrade step
    Upgrade the mysql-k8s charm using a state-driven workflow
    for safe upgrades.
    
    Adds a `sunbeam cluster refresh mysql` subcommand to trigger
    an in-channel upgrade of the mysql-k8s charm, with an optional
    `--reset-mysql-upgrade-state` flag to restart the upgrade workflow.
    
    Signed-off-by: Raven Kaur <raven.kaur@canonical.com>

--- sunbeam-python/sunbeam/commands/refresh.py
+++ sunbeam-python/sunbeam/commands/refresh.py
@@ -13,14 +13,17 @@ from sunbeam.core.juju import JujuHelper
 from sunbeam.core.manifest import AddManifestStep
 from sunbeam.steps.upgrades.base import UpgradeCoordinator
 from sunbeam.steps.upgrades.inter_channel import ChannelUpgradeCoordinator
-from sunbeam.steps.upgrades.intra_channel import LatestInChannelCoordinator
+from sunbeam.steps.upgrades.intra_channel import (
+    LatestInChannelCoordinator,
+    MySQLInChannelUpgradeCoordinator,
+)
 from sunbeam.utils import click_option_show_hints
 
 LOG = logging.getLogger(__name__)
 console = Console()
 
 
-@click.command()
+@click.group("refresh", invoke_without_command=True)
 @click.option(
     "-c",
     "--clear-manifest",
@@ -59,6 +62,9 @@ def refresh(
     Refresh the deployment. If --upgrade-release is supplied then charms are
     upgraded the channels aligned with this snap revision
     """
+    if ctx.invoked_subcommand is not None:
+        return
+
     if clear_manifest and manifest_path:
         raise click.ClickException(
             "Options manifest and clear_manifest are mutually exclusive"
@@ -92,3 +98,57 @@ def refresh(
         )
         upgrade_coordinator.run_plan(show_hints)
     click.echo("Refresh complete.")
+
+
+@refresh.command("mysql")
+@click.option(
+    "-m",
+    "--manifest",
+    "manifest_path",
+    help="Manifest file.",
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
+)
+@click.option(
+    "--reset-mysql-upgrade-state",
+    is_flag=True,
+    default=False,
+    help="Reset the mysql-k8s charm's upgrade state and start a fresh upgrade.",
+    type=bool,
+)
+@click_option_show_hints
+@click.pass_context
+def refresh_mysql(
+    ctx: click.Context,
+    manifest_path: Path | None = None,
+    reset_mysql_upgrade_state: bool = False,
+    show_hints: bool = False,
+) -> None:
+    """Upgrade mysql-k8s charm to latest revision in channel."""
+    deployment: Deployment = ctx.obj
+    client = deployment.get_client()
+    manifest = None
+    if manifest_path:
+        manifest = deployment.get_manifest(manifest_path)
+        run_plan([AddManifestStep(client, manifest_path)], console, show_hints)
+
+    if not manifest:
+        LOG.debug("Getting latest manifest from cluster db")
+        manifest = deployment.get_manifest()
+
+    if reset_mysql_upgrade_state:
+        msg = (
+            "This will reset the mysql-k8s upgrade workflow state and restart the "
+            "refresh process from the beginning.\n\n"
+            "Do you want to continue?"
+        )
+        reset_mysql_upgrade_state = click.confirm(
+            msg,
+            default=False,
+        )
+
+    jhelper = JujuHelper(deployment.juju_controller)
+    upgrade_coordinator = MySQLInChannelUpgradeCoordinator(
+        deployment, client, jhelper, manifest, reset_mysql_upgrade_state
+    )
+    upgrade_coordinator.run_plan(show_hints)
+    click.echo("MySQL refresh complete.")
