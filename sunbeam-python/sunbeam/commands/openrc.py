1cb78ca65f18e6ba1ed70ffd075ab9fcc6e83c31
Ahmad Hassan <ahmad.hassan@canonical.com>
2026-02-10 15:11:05 +0500

    feat: include region in admin openrc
    Ensure admin openrc reflects the active cloud
    region.

--- sunbeam-python/sunbeam/commands/openrc.py
+++ sunbeam-python/sunbeam/commands/openrc.py
@@ -28,7 +28,7 @@ def openrc(ctx: click.Context) -> None:
     jhelper = deployment.get_juju_helper(keystone=True)
 
     with console.status("Retrieving openrc from Keystone service ... "):
-        creds = retrieve_admin_credentials(jhelper, OPENSTACK_MODEL)
+        creds = retrieve_admin_credentials(jhelper, OPENSTACK_MODEL, client=client)
         console.print("# openrc for access to OpenStack")
         for param, value in creds.items():
             console.print(f"export {param}={value}")
