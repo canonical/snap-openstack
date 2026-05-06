42d365f94dbf3916d803a502a96524875172b38b
fdesi <francesco.desimone@canonical.com>
2026-05-06 15:17:37 +0200

    fix import sorting

--- sunbeam-python/tests/unit/sunbeam/storage/backends/conftest.py
+++ sunbeam-python/tests/unit/sunbeam/storage/backends/conftest.py
@@ -8,8 +8,8 @@ import pytest
 from sunbeam.storage.backends.dellpowerstore.backend import DellPowerstoreBackend
 from sunbeam.storage.backends.dellsc.backend import DellSCBackend
 from sunbeam.storage.backends.hitachi.backend import HitachiBackend
-from sunbeam.storage.backends.infinidat.backend import InfinidatBackend
 from sunbeam.storage.backends.hpe3par.backend import HPEthreeparBackend
+from sunbeam.storage.backends.infinidat.backend import InfinidatBackend
 from sunbeam.storage.backends.purestorage.backend import PureStorageBackend
 
 
